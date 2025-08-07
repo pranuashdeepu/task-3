b08dad47e792a9155af4736c5d01a675e7a831d8
pranuashdeepu <praneethreddynallapareddy2588@gmail.com>
2025-08-07 11:14:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+
+
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx_image" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx-container"
+  image = docker_image.nginx_image.name
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
